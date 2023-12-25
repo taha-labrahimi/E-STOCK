9ca941f7b2d6d5cb6a1c636a9f0c6f018315e3da
taha labrahimi <taha.labrahimi.devl@gmail.com>
2023-12-25 13:19:34 +0100

    add sop

--- src/e_stock/view/ClientView.java
+++ src/e_stock/view/ClientView.java
@@ -17,6 +17,7 @@ public class ClientView extends javax.swing.JFrame {
          // Initialize the database connection and repository
         DatabaseConnector dbConnector = new DatabaseConnector();
         clientRepository = new ClientRepositoryImpl(dbConnector);
+        System.out.println("e_stock.view.ClientView.<init>()");
         loadClientsAndPopulateTable();
         
     }
