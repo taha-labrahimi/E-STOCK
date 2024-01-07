22932eedcf0acc596a31b51410861f0505e3d0dd
ilyas00228 <152157784+ilyas00228@users.noreply.github.com>
2024-01-07 22:26:15 +0100

    help taha

--- src/e_stock/Model/Client.java
+++ src/e_stock/Model/Client.java
@@ -3,7 +3,7 @@ package e_stock.Model;
 public class Client {
     private Integer clientCode;
     private String firstName;
-    private String lastName;
+    private String lastName;//
     private String address;
     private String city;
     private String country;
